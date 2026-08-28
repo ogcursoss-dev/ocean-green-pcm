759d76b8a8b79a250f15b9abb317bc369d99acb1
Z User <z@container>
2026-08-28 15:15:41 +0000

    fix: remover opção eslint inválida do next.config (Next 16.3.3)

--- start-dev.sh
+++ start-dev.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-# Supervisor que mantém o dev server rodando com env correto
+# Supervisor que mantém o dev server rodando com env do Supabase
 cd /home/z/my-project
-unset DATABASE_URL DIRECT_URL
-export $(grep -v '^#' .env | xargs)
-LOG=/home/z/my-project/dev.log
 while true; do
-  echo "[$(date '+%Y-%m-%d %H:%M:%S')] Starting dev server..." >> $LOG
-  node_modules/.bin/next dev -p 3000 >> $LOG 2>&1
-  EXITCODE=$?
-  echo "[$(date '+%Y-%m-%d %H:%M:%S')] Dev server exited (code $EXITCODE), restarting in 3s..." >> $LOG
+  echo "[$(date '+%Y-%m-%d %H:%M:%S')] Starting dev server with .env.deploy..." >> /home/z/my-project/dev.log
+  bash /home/z/my-project/start-prod.sh >> /home/z/my-project/dev.log 2>&1
+  echo "[$(date '+%Y-%m-%d %H:%M:%S')] Dev server exited, restarting in 3s..." >> /home/z/my-project/dev.log
   sleep 3
 done
