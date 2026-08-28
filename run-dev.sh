ba3455fa81bd93f0185f4658b0362ebbc7054314
ogcursoss-dev <ogcursoss@gmail.com>
2026-08-28 13:41:34 -0300

    cleanup

--- run-dev.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Wrapper que inicia o supervisor em sessão isolada
-cd /home/z/my-project
-setsid bash /home/z/my-project/start-dev.sh < /dev/null > /dev/null 2>&1 &
-disown
-echo "Supervisor started (PID $!)"
